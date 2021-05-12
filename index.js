4bbb16a14d38d0b858d6482fe0b210996fd5907a
Daniel Power <danpow2013@yahoo.ca>
2021-05-12 10:35:17 -0230

    Removed test code

--- index.js
+++ index.js
@@ -2,30 +2,13 @@
 
 const buttons = document.getElementById("keyboard");
 let resultNum = document.getElementById("result");
-//let arc = document.getElementById("arc");
-//console.log(arc.checked);
-//let hyp = true;
-/*let a = "0";
-let b = "0";
-let op = "";*/
-/*document.getElementById("arc").onclick = function(){
-    if (.checked ) {
-        arc = true;
-    } else {
-        arc = false;
-    }
-}*/
 
-
-//result is top
-//previous is bottom with operators
 buttons.addEventListener('click', (event) => {
     const {target} = event;
+    console.log(target.value);
     let arc = document.getElementById("arc").checked;
     let hyp = document.getElementById("hyp").checked;
     let deg = document.getElementById("deg").checked;
-    //let rad = document.getElementById("rad").checked;
-   // console.log(target.value);
     if((target.value == "clear") || (target.value == "on")){
         document.getElementById("result").textContent = "0";
         document.getElementById("previous").textContent = "0";
@@ -60,7 +43,6 @@ buttons.addEventListener('click', (event) => {
     }
     if(target.value == "="){
         let resultExpression = document.getElementById("previous").textContent;
-        //console.log(resultExpression);
         let expression = resultExpression.slice(0,-1); //remove equals
         console.log(expression);
         answer = eval(expression);
@@ -169,142 +151,3 @@ function tan(deg,arc,hyp,input){
         return toDegrees(answer,deg);
     }
 }
-//main math function
-/*function result(a,b,operator){
-    a = parseFloat(a);
-    b = parseFloat(b);
-    let degrees = true;
-    let arc = false;
-    let hyp = false;
-
-    console.log(a);
-    console.log(operator);
-    console.log(b);
-
-    let mathfunctions = {
-
-        add: function() {
-            return a+b;
-        },
-        subtract: function() {
-            return a-b;
-        },
-        multiply: function() {
-            return a*b;
-        },
-        divide: function() {
-            return a/b;
-        },
-        inverse: function(){
-            return 1/a;
-        },
-        square: function(){
-            return a*a;
-        },
-        squareRoot: function(){
-            return Math.sqrt(a);
-        },
-        absolute: function(){
-            return Math.abs(a);
-        },
-        factorial: function(){
-            if (a === 0 || a === 1)
-                return 1;
-            for (var i = a - 1; i >= 1; i--) {
-                a *= i;
-            }
-            return a;
-        },
-        power: function(){
-            return Math.pow(a,b);
-        },
-        power10: function(){
-            return Math.pow(10,a);
-        },
-        log: function(){
-            return Math.log10(a);
-        },
-        naturalLog: function(){
-            return Math.log(a);
-        },
-        sin: function(){
-            if (degrees == true){
-                input = a * Math.PI / 180; //convert to radians
-            }
-            else if(degrees == false){ //radians
-                input = a;
-            }
-            if ((arc == true) && (hyp == true)){
-                return Math.asinh(input);
-            }
-            else if ((arc == true) && (hyp == false)){
-                return Math.asin(input);
-            }
-            else if ((arc == false) && (hyp == true)){
-                return Math.sinh(input);
-            }
-            else if ((arc == false) && (hyp == false)){
-                return Math.sin(input);
-            }
-        },
-        cos: function(){
-            if (degrees == true){
-                input = a * Math.PI / 180; //convert to radians
-            }
-            else if(degrees == false){ //radians
-                input = a;
-            }
-            if ((arc == true) && (hyp == true)){
-                return Math.acosh(input);
-            }
-            else if ((arc == true) && (hyp == false)){
-                return Math.acos(input);
-            }
-            else if ((arc == false) && (hyp == true)){
-                return Math.cosh(input);
-            }
-            else if ((arc == false) && (hyp == false)){
-                return Math.cos(input);
-            }
-        },
-        tan: function(){
-            if (degrees == true){
-                input = a * Math.PI / 180; //convert to radians
-            }
-            else if(degrees == false){ //radians
-                input = a;
-            }
-            if ((arc == true) && (hyp == true)){
-                return Math.atanh(input);
-            }
-            else if ((arc == true) && (hyp == false)){
-                return Math.atan(input);
-            }
-            else if ((arc == false) && (hyp == true)){
-                return Math.tanh(input);
-            }
-            else if ((arc == false) && (hyp == false)){
-                return Math.tan(input);
-            }
-        }
-    };
-    let answer = 0;
-    switch(operator){
-        case "+" : 
-            answer = mathfunctions.add();
-            break; 
-        case "-" :
-            answer = mathfunctions.subtract();
-            break;
-        case "*" :
-            answer = mathfunctions.multiply();
-            break;
-        case "/" :
-            answer = mathfunctions.divide();
-    }
-    document.getElementById("previous").textContent += answer;
-    document.getElementById("result").textContent = answer;
-    console.log(answer);
-}*/
-
-//document.getElementById("submit").addEventListener("click",result);
